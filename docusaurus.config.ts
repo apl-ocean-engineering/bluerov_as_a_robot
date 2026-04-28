6b530475197345097463162e3bb2f00afdaedbda
Aaron Marburg <amarburg@uw.edu>
2026-04-28 09:09:07 -0700

    Starting to work on deployment to GH pages

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -17,15 +17,17 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://apl-ocean-engineering.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/bluerov_as_a_robot/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'apl-ocean-engineering', // Usually your GitHub org/user name.
-  projectName: 'bluerov_book', // Usually your repo name.
+  projectName: 'bluerov_as_a_robot', // Usually your repo name.
+  deploymentBranch: 'main',
+  trailingSlash: 'false',
 
   onBrokenLinks: 'throw',
 
